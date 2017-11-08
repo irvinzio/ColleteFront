40ff7e6bf81ef57d22b3715cf41e9cc80e020510
Irvinzio <irvinzio.ram@gmail.com>
2017-11-07 19:23:18 -0600

    isntall and forward ports of http-server

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ clear
 echo "Installing curl"
 sudo apt-get install curl -y
 echo "Installing nodejs"
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+#curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
+npm install http-server -g
 
 
